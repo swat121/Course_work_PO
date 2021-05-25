77eb2cdf26c7f7a1eec43303503fdb03a732a377
swat121 <stark2542@gmail.com>
2021-05-25 15:33:42 +0300

    Create function filesInFolder

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,65 +1,72 @@
 package com.company;
 
 import java.io.*;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
-    public static ArrayList<File> allfiles = new ArrayList<File>();
+    public static List<File> filesInFolder = new ArrayList<File>();
+    public static Hashtable<String,List<Data>> index = new Hashtable<String,List<Data>>();
     private static final int NUMBER_THREADS = 4;
     public static void main(String[] args) throws IOException, InterruptedException {
-        File[] paths = {new File("test//neg"),new File("test//pos"),new File("train//neg"),new File("train//pos"),new File("train//unsup") };
-        initAllFiles(paths);
+        filesInFolder();
         Index[] indexThread = new Index[NUMBER_THREADS];
-        for (int i = 0; i < NUMBER_THREADS; i++) {//разбиваем на потоки
-            indexThread[i] = new Index(allfiles, allfiles.size() / NUMBER_THREADS * i,
-                    i == (NUMBER_THREADS - 1) ? allfiles.size() : allfiles.size() / NUMBER_THREADS * (i + 1), paths);
+        for (int i = 0; i < NUMBER_THREADS; i++) {
+            indexThread[i] = new Index(filesInFolder, filesInFolder.size() / NUMBER_THREADS * i,
+                    i == (NUMBER_THREADS - 1) ? filesInFolder.size() : filesInFolder.size() / NUMBER_THREADS * (i + 1));
             indexThread[i].start();
         }
-        //waiting for finish of threads
         for (int i = 0; i < NUMBER_THREADS; i++) {
             indexThread[i].join();
         }
-        String findWords = "this,horse,cat,dog";
-        Index index = new Index();
-        index.searchFiles(Arrays.asList(findWords.split(",")));
+        String findWords = "horse,cat,dog,gtrsdf";
+        searchFiles(Arrays.asList(findWords.split(",")));
+
     }
-    private static void initAllFiles(File[] paths) {
-        for (File path : paths) {
-            if (path.isDirectory()) {
-                File[] files = path.listFiles();
-                allfiles.addAll(Arrays.asList(files));
+    public static void searchFiles(List<String> words) {
+        for (String wordHigh : words) {
+            String word = wordHigh.toLowerCase();
+            List<Data> filesWithWord = index.get(word);
+            System.out.print(word+":");
+            if (filesWithWord != null) {
+                for (Data t : filesWithWord) {
+                    System.out.print(/*" позиция "+ t.positionWord+" в файле: "+*/" " +filesInFolder.get(t.fileNumber)+"; ");
+                }
             }
+            System.out.println("");
         }
+    }
+    private static void filesInFolder() throws IOException {
+        filesInFolder = Files.walk(Paths.get("dataset"))
+                .filter(Files::isRegularFile)
+                .map(Path::toFile)
+                .collect(Collectors.toList());
+        System.out.println(filesInFolder);
     }
 }
  class Index extends Thread{
-     Hashtable<String,List<Data>> index = new Hashtable<String,List<Data>>();
      int startIndex;
      int endIndex;
-     ArrayList<File> allfiles;
-     File[] paths;
-    Index(ArrayList<File> allfiles, int startIndex, int endIndex,File[] paths){
+     List<File> filesInFolder;
+    Index(List<File> allfiles, int startIndex, int endIndex){
         this.startIndex = startIndex;
         this.endIndex =endIndex;
-        this.allfiles = allfiles;
-        this.paths = paths;
+        this.filesInFolder = allfiles;
     }
-    Index(){
-
-     }
         public void run(){
-
-            int fileNumber = allfiles.indexOf(paths);
-            if (fileNumber == -1) {
-                fileNumber = allfiles.size() - 1;
-            }
             for (int i=startIndex;i<endIndex;i++){
+                int fileNumber = filesInFolder.indexOf(filesInFolder.get(i));
+                //int fileNumber = i;
+                if (fileNumber == -1) {
+                    fileNumber = filesInFolder.size() - 1;
+                }
                 int wordCounter=0;
                 BufferedReader reader = null;
                 try {
-                    reader = new BufferedReader(new FileReader(allfiles.get(i)));
+                    reader = new BufferedReader(new FileReader(filesInFolder.get(i)));
                 } catch (FileNotFoundException e) {
                     e.printStackTrace();
                 }
@@ -68,10 +75,10 @@ public class Main {
                         for (String wordHigh : line.split("\\W+")) {
                             wordCounter++;
                             String word = wordHigh.toLowerCase();
-                            List<Data> indexData = index.get(word);
+                            List<Data> indexData = Main.index.get(word);
                             if (indexData == null) {
                                 indexData = new LinkedList<Data>();
-                                index.put(word, indexData);
+                                Main.index.put(word, indexData);
                             }
                             indexData.add(new Data(fileNumber, wordCounter));
                         }
@@ -79,22 +86,9 @@ public class Main {
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-                System.out.println("indexed " + allfiles.get(i) + " " + wordCounter + " words");
+                System.out.println("indexed " + filesInFolder.get(i) + " " + wordCounter + " words");
             }
         }
-     public void searchFiles(List<String> words) {
-         for (String wordHigh : words) {
-             String word = wordHigh.toLowerCase();
-             List<Data> filesWithWord = index.get(word);
-             System.out.print(word);
-             if (filesWithWord != null) {
-                 for (Data t : filesWithWord) {
-                     System.out.print(" позиция "+ t.positionWord+" в файле: "+allfiles.get(t.fileNumber));
-                 }
-             }
-            System.out.println("");
-         }
-     }
 
 }
 class Data{
